9cbf27fc779a7d8ba72d7d76d74d6735c2b4c805
이서진 <qps0211@naver.com>
2025-09-28 23:39:30 +0900

    Clean up Project && Add sprint mission 6

--- prisma/migrations/20250810140230_add_views/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Article" ADD COLUMN     "views" INTEGER NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "public"."Product" ADD COLUMN     "views" INTEGER NOT NULL DEFAULT 0;
